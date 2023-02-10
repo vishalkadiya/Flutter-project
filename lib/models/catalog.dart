a1b135ed7861a6e553ee55fb9ab7deb18180b868
vishalkadiya <86187019+vishalkadiya@users.noreply.github.com>
2023-02-10 13:07:31 +0530

    gridview, day13

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,15 +1,5 @@
 class CatalogModel {
-  static List<Item> items = [
-    Item(
-      id: 1,
-      name: "iphone 14 pro",
-      desc: "Apple iphone 14th genration mobile",
-      price: 9999,
-      color: "#33505a",
-      image:
-          "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-pro-finish-select-202209-6-1inch-deeppurple?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1663703840578",
-    )
-  ];
+  static List<Item> items = [];
 }
 
 class Item {
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -25,6 +25,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
+    // await Future.delayed(Duration(seconds: 2));
     final catalogJson =
         await rootBundle.loadString("assets/files/catalog.json");
     final decodedData = jsonDecode(catalogJson);
@@ -47,16 +48,65 @@ class _HomePageState extends State<HomePage> {
           "PSM 100",
         ),
       ),
-      body: ListView.builder(
-        // itemCount: CatalogModel.items.length,
-        itemCount: CatalogModel.items.length,
-        itemBuilder: (context, index) {
-          return ItemWidget(
-            // item: CatalogModel.items[index],
-            item: CatalogModel.items[index],
-          );
-        },
-      ),
+      body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+              ? GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // display 2 column in one row
+                    mainAxisSpacing: 16, // give space between two row
+                    crossAxisSpacing: 16, // give space between two column
+                  ),
+                  itemBuilder: (context, index) {
+                    final item = CatalogModel.items[index];
+                    return Card(
+                        clipBehavior: Clip.antiAlias,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        child: GridTile(
+                            header: Container(
+                              padding: const EdgeInsets.all(12),
+                              decoration: const BoxDecoration(
+                                color: Colors.red,
+                              ),
+                              child: Text(
+                                item.name,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            // footer: Text(item.price.toString()),
+                            footer: Container(
+                              padding: const EdgeInsets.all(12),
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: Text(
+                                item.price.toString(),
+                                textAlign: TextAlign.right,
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            child: Image.network(item.image)));
+                  },
+                  itemCount: CatalogModel.items.length,
+                )
+              // ? ListView.builder(
+              //     // itemCount: CatalogModel.items.length,
+              //     itemCount: CatalogModel.items.length,
+              //     itemBuilder: (context, index) {
+              //       return ItemWidget(
+              //         // item: CatalogModel.items[index],
+              //         item: CatalogModel.items[index],
+              //       );
+              //     },
+              //   )
+              : const Center(
+                  child: CircularProgressIndicator(),
+                )),
       drawer: MyDrawer(),
     );
   }
